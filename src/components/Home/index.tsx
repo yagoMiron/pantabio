066f4bf4554438ac017b25d018a11ce5c11826cb
Yago Miron <yagomiron.exe@gmail.com>
2025-06-30 22:13:36 -0400

    atualizando home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,14 +1,18 @@
 import styles from "./styles.module.css";
+import downArrow from "../../assets/icons/down-arrow.svg";
 
 const Home = () => {
   return (
     <div className={styles.page}>
-      <p className={styles.description}>
-        Com a Pantabio você vai encontrar as soluções para os problemas da sua
-        lavoura, sejam pragas, doenças ou baixa produtividade, nós podemos te
-        ajudar.
-      </p>
-      <button className={styles.btn}>Descubra</button>
+      <div className={styles.content}>
+        <p className={styles.description}>
+          Com a Pantabio você vai encontrar as soluções para os problemas da sua
+          lavoura, sejam pragas, doenças ou baixa produtividade, nós podemos te
+          ajudar.
+        </p>
+        <button className={styles.btn}>Descubra</button>
+        <img src={downArrow} className={styles.arrow} />
+      </div>
     </div>
   );
 };
