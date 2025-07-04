49b0884babc41cffc468fa907499a7eac60268d8
yagoMiron <yagomiron.exe@gmail.com>
2025-07-04 18:26:18 -0400

    adaptando slider para mobile

--- src/components/Validacoes/index.tsx
+++ src/components/Validacoes/index.tsx
@@ -7,11 +7,30 @@ import card3 from "../../assets/img/Validacoes3.png";
 import card4 from "../../assets/img/Validacoes4.png";
 import card5 from "../../assets/img/Validacoes5.png";
 import card6 from "../../assets/img/Validacoes6.png";
+
+import card1Mobile from "../../assets/img/Validacoes1_mobile.png";
+import card2Mobile from "../../assets/img/Validacoes2_mobile.png";
+import card3Mobile from "../../assets/img/Validacoes3_mobile.png";
+import card4Mobile from "../../assets/img/Validacoes4_mobile.png";
+import card5Mobile from "../../assets/img/Validacoes5_mobile.png";
+import card6Mobile from "../../assets/img/Validacoes6_mobile.png";
+
 import downArrow from "../../assets/icons/down-arrow.svg";
 import InstagramCard from "../InstagramCard";
 const imagens = [card1, card2, card3, card4, card5, card6];
+const imagensMobile = [
+  card1Mobile,
+  card2Mobile,
+  card3Mobile,
+  card4Mobile,
+  card5Mobile,
+  card6Mobile,
+];
 
 const Validacoes = () => {
+  const larguraTela = window.screen.width;
+  const mobile = larguraTela <= 820;
+
   const [index, setIndex] = useState(0);
   const [fade, setFade] = useState(true);
 
@@ -45,7 +64,10 @@ const Validacoes = () => {
             fade ? styles.fadeIn : styles.fadeOut
           }`}
         >
-          <img src={imagens[index]} alt={`Imagem ${index + 1}`} />
+          <img
+            src={mobile ? imagensMobile[index] : imagens[index]}
+            alt={`Imagem ${index + 1}`}
+          />
         </div>
 
         <button className={`${styles.arrow} ${styles.right}`} onClick={proximo}>
